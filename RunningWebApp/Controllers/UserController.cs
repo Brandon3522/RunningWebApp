53ba826e368a288e9521cfc22e7e2c92d53db0b1
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-09-13 11:01:40 -0400

    Add user list page. Add user details page

---
+++ RunningWebApp/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunningWebApp.Interfaces;
+using RunningWebApp.ViewModels;
+
+namespace RunningWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+		private readonly IUserRepository _userRepository;
+
+		public UserController(IUserRepository userRepository)
+        {
+			this._userRepository = userRepository;
+		}
+        [HttpGet("users")] // Change route name to users
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            List<UserViewModel> result = new List<UserViewModel>(); // Create list of user view model
+            foreach (var user in users)
+            {
+                var userVM = new UserViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Mileage
+                };
+                result.Add(userVM);
+            }
+            return View(result);
+        }
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            var userDetailVM = new UserDetailViewModel()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Pace = user.Pace,
+                Mileage = user.Mileage
+            };
+            return View(userDetailVM);
+        }
+    }
+}
---
+++ RunningWebApp/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunningWebApp.Data;
+using RunningWebApp.Interfaces;
+using RunningWebApp.Models;
+
+namespace RunningWebApp.Repositories
+{
+	public class UserRepository : IUserRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public UserRepository(ApplicationDbContext context)
+		{
+			this._context = context;
+		}
+		public bool Add(AppUser user)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Delete(AppUser user)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<AppUser>> GetAllUsers()
+		{
+			return await _context.Users.ToListAsync(); // List of users
+		}
+
+		public async Task<AppUser> GetUserById(string id)
+		{
+			return await _context.Users.FindAsync(id); // Individual user
+		}
+
+		public bool Save()
+		{
+			var saved = _context.SaveChanges();
+			return saved > 0 ? true : false;
+		}
+
+		public bool Update(AppUser user)
+		{
+			_context.Update(user);
+			return Save();
+		}
+	}
+}
